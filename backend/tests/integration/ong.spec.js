9fee1a9ff5d61ff4bf61e5cbd74a9687b1bb6a1c
Francis Hamzagic <francishamzagic@gmail.com>
2020-03-27 15:47:46 -0300

    adding testing framework and started implementing tests

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () => {
+       const response =  await request(app)
+       .post('/ongs')
+       .send({
+           name: "Test Ong Blah",
+           email: "email@email.com",
+           whatsapp: "11111111123",
+           city: "super city",
+           uf: "uu"
+       });
+       expect(response.body).toHaveProperty('id');
+       expect(response.body.id).toHaveLength(8);
+    });
+});
